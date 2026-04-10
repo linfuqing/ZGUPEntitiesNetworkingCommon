8456716fe784258a021042d2d719b4ca4953bbb2
linfuqing <linfuqing@126.com>
2026-04-10 17:16:28 +0800

    Update NetworkRelayServer.cs

--- ZG.Entities.Networking.Common/Components/NetworkRelayServer.cs
+++ ZG.Entities.Networking.Common/Components/NetworkRelayServer.cs
@@ -188,7 +188,7 @@ namespace ZG
                 case NetworkRelayMessageType.Add:
                 {
                     uint id = reader.ReadPackedUInt(streamCompressionModel);
-                    int channelIndex = id == sendBuffer.ID ? sendBuffer.GetChannelIndex(id) : -1;
+                    int channelIndex = id == sendBuffer.ID ? -1 : sendBuffer.GetChannelIndex(id);
                     if (channelIndex != -1)
                     {
                         var identityIndex = sendBuffer.channelIndex;
